15ec4b3593cebd1689be79e7a2d27d4d20c77a7c
sangco <nsg1213@gmail.com>
2018-11-03 16:10:31 +0900

    Initial commit

---
+++ src/main/java/dev/sangco/transaction_test/TransactionTestApplication.java
@@ -0,0 +1,12 @@
+package dev.sangco.transaction_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionTestApplication.class, args);
+	}
+}
